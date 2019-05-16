47731898a880863d03849a1de9eb0d3017d8d0e0
ddang9390 <dnld1ng@yahoo.com>
2019-05-16 12:08:58 -0400

    Generated components for each main topic

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    ShoppingListEditComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
