4f96fd4ffea7d9fe8fc1163ff66340e702cb452b
ddang9390 <dnld1ng@yahoo.com>
2019-05-16 13:37:52 -0400

    Basic structure of recipe list

--- recipe-book/src/app/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipe-list/recipe-list.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes : Recipe[] = [];
+
   constructor() { }
 
   ngOnInit() {
