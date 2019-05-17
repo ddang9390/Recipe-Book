fea2544af5f56276ee4c7ba4b2fdb66395e2e07a
ddang9390 <dnld1ng@yahoo.com>
2019-05-17 16:08:52 -0400

    Added ingredients to recipe and they can be displayed

--- recipe-book/src/app/recipe.service.ts
+++ recipe-book/src/app/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe';
+import { Ingredient } from './ingredient';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,7 +9,10 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes : Recipe[] = [
-    new Recipe("test", "this is a test", "https://angular.io/assets/images/logos/angular/logo-nav@2x.png")
+    new Recipe("test", 
+      "this is a test", 
+      "https://angular.io/assets/images/logos/angular/logo-nav@2x.png",
+      [new Ingredient("meat",27)])
   ];
 
   constructor() { }
