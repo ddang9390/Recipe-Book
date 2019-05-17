b3c9344625dfd9693f753163252d0fd0924918ef
ddang9390 <dnld1ng@yahoo.com>
2019-05-17 15:51:19 -0400

    Can select recipe through service

--- recipe-book/src/app/recipe.service.ts
+++ recipe-book/src/app/recipe.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe';
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes : Recipe[] = [
     new Recipe("test", "this is a test", "https://angular.io/assets/images/logos/angular/logo-nav@2x.png")
   ];
