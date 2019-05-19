6c22e5400372d9eeeba6d1c6c7626bfecba86184
ddang9390 <dnld1ng@yahoo.com>
2019-05-19 19:07:10 -0400

    Removed unused code and replaced EventEmitter with Subject

--- recipe-book/src/app/recipe.service.ts
+++ recipe-book/src/app/recipe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe';
 import { Ingredient } from './ingredient';
@@ -7,7 +7,6 @@ import { ShoppingListService } from './shopping-list.service';
   providedIn: 'root'
 })
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes : Recipe[] = [
     new Recipe("test", 
--- recipe-book/src/app/shopping-list.service.ts
+++ recipe-book/src/app/shopping-list.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from './ingredient';
+import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -19,7 +20,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]){
