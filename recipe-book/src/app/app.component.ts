9199562fe665edbf07c9519422032f66321154b3
ddang9390 <dnld1ng@yahoo.com>
2019-05-20 16:12:00 -0400

    Using different api key and auth domain

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RecipeService } from './recipe.service';
 import { ShoppingListService } from './shopping-list.service';
 import { DataStorageService } from './data-storage.service';
 import * as firebase from 'firebase';
+
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,8 +21,8 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     firebase.initializeApp({
-      apiKey: 'AIzaSyC6A3pyWLbR-gP6QutgcQRsZG6CBdz7sgg',
-      authDomain: 'ng-recipe-book-a6367'
+      apiKey: environment.firebaseApiKey,
+      authDomain: environment.firebaseAuthDomain
     });
   }
 }
